033e8c47d38ba5ccd81353eb51c652a98811a3c6
kvin1087 <kaustubh87@gmail.com>
2017-07-25 16:22:18 -0500

    Added an IIFE (Immediately invoked function expression)

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-var createWorker = function(){
 
-    var workCount =0;
+(function() {
 
-    var task1 = function(){
-        workCount += 1;
-        console.log('task 1 ' +workCount);
+    var createWorker = function () {
+
+        var workCount = 0;
+
+        var task1 = function () {
+            workCount += 1;
+            console.log('task 1 ' + workCount);
+        };
+
+        var task2 = function () {
+            workCount += 1;
+            console.log('take 2 ' + workCount);
+        };
+
+        return {
+            job1: task1,
+            job2: task2
+        };
+
     };
 
-    var task2 = function(){
-        workCount += 1;
-        console.log('take 2' +workCount);
-    };
 
-    return {
-        job1 : task1,
-        job2: task2  
-    };
+    var worker = createWorker();
 
-};
+    worker.job1();
+    worker.job2();
+    worker.job2();
+    worker.job1();
 
-
-var worker = createWorker();
-
-worker.job1();
-worker.job2();
-worker.job2();
-worker.job1();
+}());
