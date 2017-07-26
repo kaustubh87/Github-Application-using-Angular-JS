9ecd1218c3e8ec47cc3f48078814427f7d35ab58
kvin1087 <kaustubh87@gmail.com>
2017-07-26 10:16:47 -0500

    Added error function

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -2,9 +2,13 @@ var app = angular.module('github-viewer', []);
 
 app.controller("MainController", function($scope, $http){
 
-        $http.get('https://api.github.com/users/kaustubh87')
+        $http.get('https://api.github.com/users/kausubh87')
                 .then(function(response){
                      $scope.user = response.data;   
+                }, function(error){
+                    if(error){
+                        $scope.error = "Could not fetch the user";
+                    }
                 });
 
         
