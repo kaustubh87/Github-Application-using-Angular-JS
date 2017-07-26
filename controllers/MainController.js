c960f3c17ded2022a45c80f0547b177978ef7b6b
kvin1087 <kaustubh87@gmail.com>
2017-07-26 15:37:53 -0500

    Added Main Controller

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -2,14 +2,20 @@ var app = angular.module('github-viewer', []);
 
 app.controller("MainController", function($scope, $http){
 
-        $http.get('https://api.github.com/users/kausubh87')
-                .then(function(response){
-                     $scope.user = response.data;   
-                }, function(error){
-                    if(error){
-                        $scope.error = "Could not fetch the user";
-                    }
-                });
+        var success = function(response){
+            $scope.user = response.data;
+        };
 
+        var error = function(error){
+             $scope.error = "Could not fetch the user";
+        };
+
+
+
+        $scope.search = function(){
+        var username = $scope.username;
+        $http.get('https://api.github.com/users/'+username)
+                .then(success, error);
+        };
         
 });
