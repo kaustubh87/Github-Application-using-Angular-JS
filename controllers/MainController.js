b58fa214300a3a7d51e7ec4c4c5fcacb07f43d67
kvin1087 <kaustubh87@gmail.com>
2017-07-27 10:49:04 -0500

    Added $interval service for countdown

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,28 +1,43 @@
 var app = angular.module('github-viewer', []);
 
-app.controller("MainController", function($scope, $http){
+app.controller("MainController", function ($scope, $http, $interval) {
 
-        var success = function(response){
-            $scope.user = response.data;
-            $http.get($scope.user.repos_url)
-                .then(onRepos, error);
-        };
+    var success = function (response) {
+        $scope.user = response.data;
+        $http.get($scope.user.repos_url)
+            .then(onRepos, error);
+    };
 
-        var onRepos = function(response){
-            $scope.repos = response.data;
-        };
+    var onRepos = function (response) {
+        $scope.repos = response.data;
+    };
 
-        var error = function(error){
-             $scope.error = "Could not fetch the user";
-        };
+    var error = function (error) {
+        $scope.error = "Could not fetch the user";
+    };
 
+    var decrementCountdown = function () {
+        $scope.countdown -= 1;
+        if ($scope.countdown < 1) {
+            $scope.search($scope.username);
+        }
+    };
 
-        $scope.search = function(){
+    var startCountdown = function () {
+        $interval(decrementCountdown, 1000, $scope.countdown);
+    };
+
+    $scope.search = function () {
         var username = $scope.username;
-        $http.get('https://api.github.com/users/'+username)
-                .then(success, error);
-        };
+        $http.get('https://api.github.com/users/' + username)
+            .then(success, error);
+    };
 
-        $scope.sortOrderbyLanguage = '-language';
-        
+    $scope.sortOrderbyLanguage = '-language';
+    $scope.username = "kaustubh87";
+    $scope.countdown = 5;
+    startCountdown();
+
+
+
 });
